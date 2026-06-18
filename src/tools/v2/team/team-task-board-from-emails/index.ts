9611a0e9fdaae0913d76fb0e022a2401ae0031a3
mxrtins04 <olatunbosunmartins14@gmail.com>
2026-06-18 10:28:32 +0100

    feat(tools): add team-task-board-from-emails core logic

---
+++ src/tools/v2/team/team-task-board-from-emails/index.ts
@@ -0,0 +1,3 @@
+export { parseEmailsToTasks, groupByAssignee, groupByStatus } from "./parser";
+export { mockEmails } from "./fixtures";
+export type { EmailFixture, Task, TaskStatus, ParseResult, ParseError, LoadingState } from "./types";
